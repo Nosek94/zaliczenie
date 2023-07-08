8b8887d906e1eb164ba63ebfb77efae3157703e8
Nosek94 <72353069+Nosek94@users.noreply.github.com>
2023-07-08 22:23:49 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Zadanie zaliczeniowe - app service</title>
+</head>
+<body>
+    <h1>Zadanie zaliczeniowe - app service</h1>
+    <p><?php echo date('d.m.Y'); ?>, Adrian Noske</p>
+</body>
+</html>
